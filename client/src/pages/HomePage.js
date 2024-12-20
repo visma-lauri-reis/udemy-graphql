0df44de517e93fe8691c2aab0d86eecae7525aa6
Lauri Reis <lauri.reis@visma.com>
2024-12-20 13:44:21 +0200

    Lesson 24

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,13 @@
 import JobList from '../components/JobList';
 import { jobs } from '../lib/fake-data';
+import { getJobs } from '../lib/graphql/queries';
+
+getJobs().then((jobs) => console.log(jobs));
 
 function HomePage() {
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );

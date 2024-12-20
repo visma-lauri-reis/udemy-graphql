e58ad7c3de7379af553ee207d24416dd1d24a2ab
Lauri Reis <lauri.reis@visma.com>
2024-12-20 11:16:59 +0200

    Lesson 17

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,18 @@
 export const resolvers = {
   Query: {
-    job: () => {
-      return {
-        id: 'test-id',
-        title: 'The Title',
-        description: 'The Description',
-      };
+    jobs: () => {
+      return [
+        {
+          id: 'test-id-1',
+          title: 'The Title 1',
+          description: 'The Description',
+        },
+        {
+          id: 'test-id-2',
+          title: 'The Title 2',
+          description: 'The Description',
+        },
+      ];
     },
   },
 };

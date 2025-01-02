b5ae6ccc0b1843e5e7da94b9457ef133c0d3358b
Lauri Reis <lauri.reis@visma.com>
2025-01-02 14:37:29 +0200

    Lesson 36

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,5 @@
 import { GraphQLError } from 'graphql';
-import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
+import { createJob, getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
 
 export const resolvers = {
@@ -19,6 +19,13 @@ export const resolvers = {
       return job;
     },
     jobs: () => getJobs(),
+  },
+
+  Mutation: {
+    createJob: (_root, { title, description }) => {
+      const companyId = 'FjcJCHJALA4i'; // TODO: set based on user
+      return createJob({ companyId, title, description });
+    },
   },
 
   Company: {

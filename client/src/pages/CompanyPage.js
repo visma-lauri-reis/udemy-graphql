abd2cbd1a1e75e5ac8b1dfba76d46d73d1abe6e1
Lauri Reis <lauri.reis@visma.com>
2025-01-02 14:15:53 +0200

    Lesson 34

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -5,13 +5,27 @@ import JobList from '../components/JobList.js';
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const [company, setCompany] = useState();
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false,
+  });
 
   useEffect(() => {
-    getCompany(companyId).then(setCompany);
+    (async () => {
+      try {
+        const company = await getCompany(companyId);
+        setState({ company, loading: false, error: false });
+      } catch (error) {
+        setState({ company: null, loading: false, error: true });
+      }
+    })();
   }, [companyId]);
 
-  if (!company) return <div>Loading...</div>;
+  const { company, loading, error } = state;
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div className="has-text-danger">Data unavailable</div>;
 
   return (
     <div>

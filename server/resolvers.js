32e3ab55de90c041301c427e77ed112a7e8e24e0
Lauri Reis <lauri.reis@visma.com>
2025-01-02 15:41:51 +0200

    Lesson 45

--- server/resolvers.js
+++ server/resolvers.js
@@ -29,12 +29,11 @@ export const resolvers = {
   },
 
   Mutation: {
-    createJob: (_root, { input: { title, description } }, { auth }) => {
-      if (!auth) {
+    createJob: (_root, { input: { title, description } }, { user }) => {
+      if (!user) {
         throw unauthorizedError('Missing authentication');
       }
-      const companyId = 'FjcJCHJALA4i'; // TODO: set based on user
-      return createJob({ companyId, title, description });
+      return createJob({ companyId: user.companyId, title, description });
     },
     deleteJob: (_root, { id }) => deleteJob(id),
     updateJob: (_root, { input: { id, title, description } }) => {

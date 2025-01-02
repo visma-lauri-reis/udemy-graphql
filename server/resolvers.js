27c30b62b7fd8c795fd1ad2e4765d55073255b34
Lauri Reis <lauri.reis@visma.com>
2025-01-02 12:29:40 +0200

    Lesson 28

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,6 +3,7 @@ import { getCompany } from './db/companies.js';
 
 export const resolvers = {
   Query: {
+    company: (_root, { id }) => getCompany(id),
     job: (_root, { id }) => getJob(id),
     jobs: () => getJobs(),
   },

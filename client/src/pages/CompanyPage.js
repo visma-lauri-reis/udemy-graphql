307cc1e265eb243ea9a41e0ac44829d5aa273d35
Lauri Reis <lauri.reis@visma.com>
2025-01-02 12:43:02 +0200

    Lesson 29

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router';
 import { useEffect, useState } from 'react';
 import { getCompany } from '../lib/graphql/queries';
+import JobList from '../components/JobList.js';
 
 function CompanyPage() {
   const { companyId } = useParams();
@@ -16,6 +17,8 @@ function CompanyPage() {
     <div>
       <h1 className="title">{company.name}</h1>
       <div className="box">{company.description}</div>
+      <h2 className="title is-5">Jobs at {company.name}</h2>
+      <JobList jobs={company.jobs} />
     </div>
   );
 }
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,4 @@
-import { getJob, getJobs } from './db/jobs.js';
+import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
 
 export const resolvers = {
@@ -6,6 +6,10 @@ export const resolvers = {
     company: (_root, { id }) => getCompany(id),
     job: (_root, { id }) => getJob(id),
     jobs: () => getJobs(),
+  },
+
+  Company: {
+    jobs: (company) => getJobsByCompany(company.id),
   },
 
   Job: {

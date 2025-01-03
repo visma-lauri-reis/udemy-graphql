9aba12cfd85cc487f2200a645a98eed94507072b
Lauri Reis <lauri.reis@visma.com>
2025-01-03 11:22:12 +0200

    Lesson 56

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -94,6 +94,9 @@ export async function getJobs() {
       }
     }
   `;
-  const { data } = await apolloClient.query({ query });
+  const { data } = await apolloClient.query({
+    query,
+    fetchPolicy: 'network-only',
+  });
   return data.jobs;
 }

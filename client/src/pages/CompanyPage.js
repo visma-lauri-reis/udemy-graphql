8605940eb767897c74c2163b886400eb8cc578f8
Lauri Reis <lauri.reis@visma.com>
2025-01-03 12:53:48 +0200

    Lesson 61

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,18 +1,14 @@
 import { useParams } from 'react-router';
-import { companyByIdQuery } from '../lib/graphql/queries';
 import JobList from '../components/JobList.js';
-import { useQuery } from '@apollo/client';
+import { useCompany } from '../lib/graphql/hooks.js';
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const { data, loading, error } = useQuery(companyByIdQuery, {
-    variables: { id: companyId },
-  });
+  const { company, loading, error } = useCompany(companyId);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div className="has-text-danger">Data unavailable</div>;
 
-  const { company } = data;
   return (
     <div>
       <h1 className="title">{company.name}</h1>

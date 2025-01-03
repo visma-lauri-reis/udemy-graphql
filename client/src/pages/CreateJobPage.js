14e7e9dd6143d760603477960a70bf20cd699e9b
Lauri Reis <lauri.reis@visma.com>
2025-01-03 13:21:03 +0200

    Lesson 63

--- client/src/pages/CreateJobPage.js
+++ client/src/pages/CreateJobPage.js
@@ -1,16 +1,30 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
-import { createJob } from '../lib/graphql/queries';
+import { createJobMutation, jobByIdQuery } from '../lib/graphql/queries';
+import { useMutation } from '@apollo/client';
 
 function CreateJobPage() {
   const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const [mutate] = useMutation(createJobMutation);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const job = await createJob({ title, description });
-    console.log('job created:', { title, description });
+    const {
+      data: { job },
+    } = await mutate({
+      variables: {
+        input: { title, description },
+        update: (cache, { data }) => {
+          cache.writeQuery({
+            query: jobByIdQuery,
+            variables: { id: data.job.id },
+            data,
+          });
+        },
+      },
+    });
     navigate(`/jobs/${job.id}`);
   };
 
